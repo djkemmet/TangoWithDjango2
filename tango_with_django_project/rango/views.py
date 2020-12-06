f14a355a3e195f9926665ef3251b63b96bd67f5f
DJ <kemmetdaniel@gmail.com>
2020-12-06 15:09:07 -0600

    Module 3 Challenges Complete.
    Made a new page, created links to from that page to home.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,6 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("""
+    
+    <h1>Rango says hey there partner!</h1>
+    <a href="/rango/about/">About Page</a>
+    
+    
+    """)
 
+def about(request):
+    return HttpResponse("""
+    
+    
+    <h1>Rango says here is the about page.</h1>
+    <a href="/">Home</a>
+
+    """)
 
