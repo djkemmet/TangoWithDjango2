9448abbb792616c536c60e065da05d6badc0ce2e
DJ <kemmetdaniel@gmail.com>
2020-12-06 15:04:12 -0600

    3.5 Mapping URLs
    In this module we basically learned how to route to indiviidual applications and let the that application's newly created urls.py file handle the view routing for us.

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rango import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+
+    # map any URLS start with rango/ to be handled by the rango app
+    path('rango/', include('rango.urls'))
 
 ]
