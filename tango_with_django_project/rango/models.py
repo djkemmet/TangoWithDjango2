e2a9aaa178704e484572e13d58a856700d7ec563
DJ Kemmet <kemmetdaniel@gmail.com>
2020-12-25 14:12:11 -0600

    5.3 Creating Models
    Went through making 2 models and linking them together with a forigen key, then the rest of the section was spent explaining field types, and then the  __str__() method so that the models could be represented in the interface by their relevant properties, name.

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+#Category Model
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    # When we reference this object in the GUI / application, what will the String
+    # representing the instance of a particular category model be set to? in this case, 
+    # it will be the name property
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+
+    # The category property of the Page model is a reference to the Category
+    # 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
